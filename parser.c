419a444fc078e6df02651e4ddf9e31151927f09a
csd4144 <csd4144@csd.uoc.gr>
2022-02-24 21:18:17 +0200

    added structs

--- parser.c
+++ parser.c
@@ -9,28 +9,35 @@ enum READING_ENUM reading_state;
 int total_contributors;
 int total_projects;
 
-char * get_word(char * line){
-    while(line != EOF){
-        if(line == ' '){
-            return line;
-        }
+int current_contributor = 0;
+char current_contributor_name[20];
 
-        line++;
-    }
-    return NULL;
-}
+int current_skills = 0;
+char current_skill_name[20];
+
+
+int current_project = 0;
+
+
+
+
+
 
 
 void process_line(char * line){
+   
     if(reading_state == READING_HEADER){
-        
+        sscanf( line, "%d %d", total_contributors, total_projects );
+        reading_state = READING_CONTRIBUTOR;
 
     }else if(reading_state == READING_CONTRIBUTOR){
-
+        sscanf( line, "%s %d", current_contributor_name, current_skills );
+        reading_state = READING_SKILLS;
 
     }else if(reading_state == READING_SKILLS){
+        
 
-
+        
     }else if(reading_state == READING_PROJECT){
 
 
---
+++ structs.h
@@ -0,0 +1,15 @@
+struct project{
+    char name[20];
+    int days;
+    int score;
+    int best_before;
+    int number_of_roles;
+    char ** contributors;
+
+};
+
+
+struct contributor{
+    char name[20];
+
+};
